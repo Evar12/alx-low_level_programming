0d5666c665a56ad68aff6b46440736554f592aa0
Evar12 <Evaristus12@gmail.com>
2023-09-12 21:38:01 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * Main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		printf:(text[i]);
+	}
+	_putchar:('\n');
+
+	return (0);
+
+}
