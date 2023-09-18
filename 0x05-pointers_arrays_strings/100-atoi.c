76124709d9bcbbf5505b2394e910264c74d99f91
Evar12 <Evaristus12@gmail.com>
2023-09-18 08:45:42 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * _atoi - convert a string to an integer.
@@ -7,45 +9,18 @@
  * Return: Always 0
  */
 
-/*int _atoi(char *s) 
-{
-	char num[50];
-	int  i, len;
-	int result = 0;
-
-	printf("Enter a number: ");
-	gets(num);
-	len = strlen(num);
-
-	for(i=0; i<len; i++)
-	{
-		result = result * 10 + ( num[i] - '0' );
-	}
-
-	printf("%d", result);
-
-}
-*/
 int _atoi(char *s)
 {
-	int nb;
+	int val;
+	char str[20];
 
-	nb = _atoi("98");
-	printf("%d\n", nb);
-	nb = _atoi("-402");
-	printf("%d\n", nb);
-	nb = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", nb);
-	nb = _atoi("214748364");
-	printf("%d\n", nb);
-	nb = _atoi("0");
-	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
-	printf("%d\n", nb);
-	nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", nb);
+	strcpy(str, "98993489");
+	val = atoi(str);
+	printf("String value = %s, Int value = %d\n", str, val);
+
+	strcpy(str, "tutorialspoint.com");
+	val = atoi(str);
+	printf("String value = %s, Int value = %d\n", str, val);
+
 	return (0);
 }
-
