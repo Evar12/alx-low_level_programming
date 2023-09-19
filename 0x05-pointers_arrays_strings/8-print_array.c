563949f613c88124dbe15bbd49db7a0a3ec8f9d8
Evar12 <Evaristus12@gmail.com>
2023-09-19 08:55:09 -0700

    project 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,22 +9,28 @@
  */
 
 void print_array(int *a, int n)
-{
-	int i, count;
 
-	i = 0;
-	count = 1;
-	if (n < 0)
-		n = 0;
-	if (n > 0)
+{
+
+	int i;
+
+
+	for (i = 0; i < (n - 1); i++)
+
 	{
-		while (a[i] != '\0' && count < n)
-		{
-			printf("%d, ", a[i]);
-			i++;
-			count++;
-		}
-		printf("%d\n, ", a[i]);
+
+		printf("%d, ", a[i]);
 
 	}
+
+	if (i == (n - 1))
+
+	{
+
+		printf("%d", a[n - 1]);
+
+	}
+
+	printf("\n");
+
 }
