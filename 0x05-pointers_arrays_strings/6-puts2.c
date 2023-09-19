977a04020c320994152309ccb08e2c2d7a0075d2
Evar12 <Evaristus12@gmail.com>
2023-09-19 03:49:29 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,56 @@
-#includ "main.h"
+#include "main.h"
 
 /**
- *puts2 - print alternating chars of string
- * @str: string
- * Return: void
+
+ * puts2 - function should print only one character out of two
+
+ * starting with the first one
+
+ * @str: input
+
+ * Return: print
+
  */
 
-void puts(char *str)
-{
-	int i;
+void puts2(char *str)
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (1 % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+{
+
+int longi = 0;
+
+int t = 0;
+
+char *y = str;
+
+int o;
+
+
+while (*y != '\0')
+
+{
+
+y++;
+
+longi++;
+
+}
+
+t = longi - 1;
+
+for (o = 0 ; o <= t ; o++)
+
+{
+
+if (o % 2 == 0)
+
+{
+
+_putchar(str[o]);
+
+}
+
+}
+
+_putchar('\n');
+
 }
