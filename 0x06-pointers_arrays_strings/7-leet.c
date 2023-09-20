79fcd7d0131cd6a18d3ba7e4e2ac1dd6add3dbe1
Evar12 <Evaristus12@gmail.com>
2023-09-20 15:40:37 -0700

    7-encode string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,23 +6,21 @@
  * Return: string array
  */
 
-char *leet(char *s)
+char *leet(char *n)
 {
-	int i;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (n[i] == s1[j])
 		{
-	if (s[i] == 'a' || s[i] == 'A')
-		s[i] = '4';
-	else (s[i] == 'e' || s[i] == 'E')
-		s[i] = '3';
-	else (s[i] == 'o' || s[i] == 'O')
-		s[i] = '0';
-	else (s[i] == 't' || s[i] == 'T')
-		s[i] = '7';
-	else (s[i] == 'l' || s[i] == 'L')
-		s[i] = '1';
+		n[i] = s2[j];
+		}
 	}
-
-	return (s);
+	}
+	return (n);
 }
