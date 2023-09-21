46550fdeda888dea11024ba1c4c951b60ed61214
Evar12 <Evaristus12@gmail.com>
2023-09-21 02:03:06 -0700

    encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - encoder rot13
@@ -16,11 +17,12 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (j = 0; j < 52; j++)
-	{
-	if (s[i] == data1[j])
-		s[i] = datarot[j];
-		break;
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
 			}
 		}
 	}
